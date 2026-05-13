8c581f54edf30484d8c4f0d0637b0a8c4c281649
ogedz <ogedengbe_m@yahoo.com>
2026-05-13 14:57:10 +0100

    edit based on the rubric

--- scripts/temples.js
+++ scripts/temples.js
@@ -9,4 +9,6 @@ const navigation = document.querySelector('.navigation');
 hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
+    const isExpanded = hamButton.getAttribute('aria-expanded') === 'true';
+    hamButton.setAttribute('aria-expanded', !isExpanded);
 });
