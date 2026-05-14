c184c45f68ef18ea5df5a813e63ae72f093a0a4a
ogedz <ogedengbe_m@yahoo.com>
2026-05-14 14:01:12 +0100

    updated version

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,8 +1,3 @@
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
-
-/* Hamburger Menu Toggle */
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
